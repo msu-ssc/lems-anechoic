2d71024f5c7beee448827d959654b5187892006e
David Mayo <dcmayo@moreheadstate.edu>
2025-02-17 09:10:44 -0500

    Create GpibDevice base class

--- msu_anechoic/spec_an.py
+++ msu_anechoic/spec_an.py
@@ -1,5 +1,6 @@
 from __future__ import annotations
 
+import contextlib
 import os
 import sys
 from typing import TYPE_CHECKING
@@ -8,7 +9,7 @@ from typing import Literal
 import numpy as np
 import pyvisa
 
-sys.path.append(os.path.abspath("."))   # SO STUPID!
+sys.path.append(os.path.abspath("."))  # SO STUPID!
 
 import msu_anechoic
 
@@ -16,17 +17,11 @@ if TYPE_CHECKING:
     import logging
 
 
-__all__ = ["SpectrumAnalyzerHP8563E"]
+__all__ = ["GpibDevice", "SpectrumAnalyzerHP8563E"]
 
 
-class SpectrumAnalyzerHP8563E:
-    """Class for interacting with a Hewlett Packard 8563E Spectrum Analyzer via GPIB.
-
-    This class only implements a few commands. There are many more available. Detailed information for this model is
-    in "User's Guide Agilent Technologies 8560 E-Series and EC-Series Spectrum Analyzers", which you can Google. As of
-    2025-02-13, you can find it at
-    https://testworld.com/wp-content/uploads/user-guide-keysight-agilent-8561e-8562e-8563e-8564e-8565e-8561ec-8562ec-8563ec-8564ec-8565ec-spectrum-analyzers.pdf
-    """
+class GpibDevice:
+    """A base class for GPIB devices at the MSU Space Science Center."""
 
     # This is what's called when you do `SpecAnHP8563E("GPIB0::18::INSTR")`.
     def __init__(
@@ -36,10 +31,12 @@ class SpectrumAnalyzerHP8563E:
         open_immediately: bool = True,
         resource_manager: pyvisa.highlevel.ResourceManager | None = None,
         logger: logging.Logger | None = None,
+        log_query_messages: bool = False,
     ):
         self.logger = logger or msu_anechoic._create_null_logger()
         self.gpib_address = gpib_address
         self.resource_manager = resource_manager or pyvisa.ResourceManager()
+        self.log_every_message = log_query_messages
 
         self.logger.debug(f"SpecAn object created at {self.gpib_address=} and {self.resource_manager=}")
 
@@ -52,145 +49,58 @@ class SpectrumAnalyzerHP8563E:
         """Query the spectrum analyzer.
 
         Return the response, which will be a string, usually with trailing `'\\n'`."""
-        # self.logger.debug(f"Querying {self.gpib_address=} with {command=}")
+        if self.log_every_message:
+            self.logger.debug(f"Querying {self.gpib_address!r} with {command=}")
         if self.resource is None:
             raise ValueError("Connection to the spectrum analyzer is not open.")
         response = self.resource.query(command)
-        # self.logger.debug(f"Response from {self.gpib_address=} to {command=} is {response=}")
+        if self.log_every_message:
+            self.logger.debug(f"Response from {self.gpib_address!r} to {command=} is {response=}")
         return response
 
     def write(self, command: str) -> int:
         """Write to the spectrum analyzer.
 
         Return the number of bytes written."""
-        # self.logger.debug(f"Writing {command=} to {self.gpib_address=}")
+        if self.log_every_message:
+            self.logger.debug(f"Writing {command=} to {self.gpib_address!r}")
         if self.resource is None:
             raise ValueError("Connection to the spectrum analyzer is not open.")
         response = self.resource.write(command)
-        # self.logger.debug(f"{command=} written to {self.gpib_address=} with {response=}")
+        if self.log_every_message:
+            self.logger.debug(f"{command=} written to {self.gpib_address!r} with {response=}")
         return response
-
-    @classmethod
-    def find(cls) -> "SpectrumAnalyzerHP8563E | None":
-        """Find the spectrum analyzer."""
-        resource_manager = pyvisa.ResourceManager()
-        resources = resource_manager.list_resources()
-        for resource in resources:
-            try:
-                resource: pyvisa.resources.MessageBasedResource = resource_manager.open_resource(resource)
-                response = resource.query("CF?")
-            except Exception as exc:
-                continue
-            if response:
-                return SpectrumAnalyzerHP8563E(
-                    gpib_address=resource.resource_name,
-                )
-        return None
-
-    def set_center_frequency(self, frequency: float):
-        """Set the center frequency of the spectrum analyzer in Hertz."""
-        self.logger.debug(f"Setting center frequency of {self.gpib_address=} to {frequency=}")
-        self.write(f"CF {frequency}")
-
-    def get_center_frequency(self) -> float:
-        """Get the center frequency of the spectrum analyzer in Hertz."""
-        self.logger.debug(f"Getting center frequency of {self.gpib_address=}")
-        response = self.query("CF?")
-        return float(response)
-
-    def set_span(self, span: float):
-        """Set the span of the spectrum analyzer in Hertz."""
-        self.logger.debug(f"Setting span of {self.gpib_address=} to {span=}")
-        self.write(f"SP {span}")
-
-    def get_span(self) -> float:
-        """Get the span of the spectrum analyzer in Hertz."""
-        self.logger.debug(f"Getting span of {self.gpib_address=}")
-        return float(self.query("SP?").strip())
-
-    def get_lower_frequency(self) -> float:
-        """Get the lower frequency of the spectrum analyzer in Hertz."""
-        self.logger.debug(f"Getting lower frequency of {self.gpib_address=}")
-        return float(self.query("FA?"))
-
-    def get_upper_frequency(self) -> float:
-        """Get the upper frequency of the spectrum analyzer in Hertz."""
-        self.logger.debug(f"Getting upper frequency of {self.gpib_address=}")
-        return float(self.query("FB?"))
-
-    def set_lower_frequency(self, frequency: float):
-        """Set the lower frequency of the spectrum analyzer in Hertz."""
-        self.logger.debug(f"Setting lower frequency of {self.gpib_address=} to {frequency=}")
-        self.write(f"FA {frequency}")
-
-    def set_upper_frequency(self, frequency: float):
-        """Set the upper frequency of the spectrum analyzer in Hertz."""
-        self.logger.debug(f"Setting upper frequency of {self.gpib_address=} to {frequency=}")
-        self.write(f"FB {frequency}")
-
-    def get_trace(self, trace: Literal["A", "B"] = "A") -> np.ndarray[np.float64]:
-        """Get the trace from the spectrum analyzer."""
-        self.logger.debug(f"Getting trace {trace} from {self.gpib_address=}")
-        response = self.query(f"TR{trace}?")
-        return np.array([float(x) for x in response.split(",")])
-
-    def get_trace_frequencies_and_amplitudes(self, trace: Literal["A", "B"] = "A") -> tuple[np.ndarray[np.float64], np.ndarray[np.float64]]:
-        """Get the frequencies and amplitudes of the trace from the spectrum analyzer.
-        
-        Response will be a tuple of Numpy arrays: `(frequencies, amplitudes)`."""
-        import numpy as np
-        self.logger.debug(f"Getting trace frequencies and amplitudes {trace} from {self.gpib_address=}")
-        amplitudes = self.get_trace(trace)
-        # Have to manually calculate the frequencies because the spectrum analyzer doesn't provide them.
-        frequencies = np.linspace(self.get_lower_frequency(), self.get_upper_frequency(), len(amplitudes))
-        return frequencies, amplitudes
-
-    def get_marker_frequency(self) -> float:
-        """Get the frequency of the marker in Hertz."""
-        self.logger.debug(f"Getting marker frequency from {self.gpib_address=}")
-        return float(self.query(f"MKF?"))
-
-    def set_marker_frequency(self, frequency: float):
-        """Set the frequency of the marker in Hertz."""
-        self.logger.debug(f"Setting marker frequency of {self.gpib_address=} to {frequency=}")
-        self.write(f"MKF {frequency}")
-
-    def get_marker_amplitude(self) -> float:
-        """Get the amplitude of the marker."""
-        self.logger.debug(f"Getting marker amplitude from {self.gpib_address=}")
-        return float(self.query(f"MKA?"))
-
-    def get_center_frequency_amplitude(self) -> float:
-        """Get the amplitude at the center frequency of the spectrum analyzer.
-
-        Does three things:
-        1. Gets the center frequency.
-        2. Sets the marker frequency to the center frequency.
-        3. Gets the marker amplitude."""
-        self.logger.debug(f"Getting center frequency amplitude from {self.gpib_address=}")
-        center_frequency = self.get_center_frequency()
-        self.set_marker_frequency(center_frequency)
-        return self.get_marker_amplitude()
 
     def open(self):
         """Open the connection to the spectrum analyzer."""
-        self.logger.info(f"Attempting to open GPIB connection to {self.gpib_address=}")
+        self.logger.info(f"Attempting to open GPIB connection to {self.gpib_address!r}")
         try:
             self.resource = self.resource_manager.open_resource(self.gpib_address)
-            self.logger.info(f"Connection to {self.gpib_address=} opened. {self.resource=}")
+            self.logger.info(f"Connection to {self.gpib_address!r} opened. {self.resource=}")
         except Exception as exc:
             self.logger.error(f"Error opening {self!r}. {exc}", exc_info=True)
             raise
 
     def close(self):
         """Close the connection to the spectrum analyzer."""
-        self.logger.debug(f"Closing connection to {self.gpib_address=}")
+        self.logger.debug(f"Closing connection to {self.gpib_address!r}")
         if self.resource:
             try:
                 self.resource.close()
-                self.logger.info(f"Connection to {self.gpib_address=} closed.")
+                self.logger.info(f"Connection to {self.gpib_address!r} closed.")
             except Exception as exc:
                 self.logger.error(f"Error closing {self!r}. {exc}", exc_info=True)
+
+    @classmethod
+    def find(
+        cls,
+        *,
+        logger: logging.Logger | None = None,
+        resource_manager: pyvisa.highlevel.ResourceManager | None = None,
+        open_immediately: bool = True,
+        log_query_messages: bool = False,
+    ) -> "SpectrumAnalyzerHP8563E | None":
+        raise NotImplementedError(f"This method must be implemented in a subclass")
 
     # This is a method to make this a context manager. (with ... as ...).
     # Don't worry about this.
@@ -205,6 +115,159 @@ class SpectrumAnalyzerHP8563E:
     # This makes the object more readable when printed.
     def __repr__(self):
         return f"{self.__class__.__name__}(gpib_address={self.gpib_address!r})"
+
+    pass
+
+
+class SpectrumAnalyzerHP8563E(GpibDevice):
+    """Class for interacting with a Hewlett Packard 8563E Spectrum Analyzer via GPIB.
+
+    This class only implements a few commands. There are many more available. Detailed information for this model is
+    in "User's Guide Agilent Technologies 8560 E-Series and EC-Series Spectrum Analyzers", which you can Google. As of
+    2025-02-13, you can find it at
+    https://testworld.com/wp-content/uploads/user-guide-keysight-agilent-8561e-8562e-8563e-8564e-8565e-8561ec-8562ec-8563ec-8564ec-8565ec-spectrum-analyzers.pdf
+    """
+
+    @classmethod
+    def find(
+        cls,
+        *,
+        logger: logging.Logger | None = None,
+        resource_manager: pyvisa.highlevel.ResourceManager | None = None,
+        open_immediately: bool = True,
+        log_query_messages: bool = False,
+    ) -> "SpectrumAnalyzerHP8563E | None":
+        """Find an HP 8563E spectrum analyzer on the GPIB bus.
+
+        Return a `SpectrumAnalyzerHP8563E` object if one is found, otherwise return None.
+
+
+        Example:
+        ```python
+        with SpectrumAnalyzerHP8563E.find() as spectrum_analyzer:
+            if not spectrum_analyzer:
+                logger.error("No spectrum analyzer found.")
+                sys.exit(1)
+            center_frequency = spectrum_analyzer.get_center_frequency()
+            print(f")
+        ```
+        """
+        resource_manager = resource_manager or pyvisa.ResourceManager()
+        resources = resource_manager.list_resources()
+        for resource in resources:
+            if logger:
+                logger.debug(f"Checking {resource=}")
+            try:
+                resource: pyvisa.resources.MessageBasedResource = resource_manager.open_resource(resource)
+
+                # NOTE: 8563E doesn't respond to "*IDN?", so we have to use something else.
+                # Of the GPIB devices at MSC Space Science Center, this is the only one that responds to "CF?".
+                response = resource.query("CF?")
+            except Exception as exc:
+                if logger:
+                    logger.debug(f"Error checking {resource=}. {exc}")
+                continue
+            if response:
+                if logger:
+                    logger.info(f"Found HP 8563E spectrum analyzer at {resource=}")
+                return SpectrumAnalyzerHP8563E(
+                    gpib_address=resource.resource_name,
+                    logger=logger,
+                    resource_manager=resource_manager,
+                    open_immediately=open_immediately,
+                    log_query_messages=log_query_messages,
+                )
+
+        # nullcontext is a context manager that does nothing. It evaluates to `None` in an `as` clause.
+        return contextlib.nullcontext()
+
+    def set_center_frequency(self, frequency: float):
+        """Set the center frequency of the spectrum analyzer in Hertz."""
+        self.logger.debug(f"Setting center frequency of {self.gpib_address!r} to {frequency=}")
+        self.write(f"CF {frequency}")
+
+    def get_center_frequency(self) -> float:
+        """Get the center frequency of the spectrum analyzer in Hertz."""
+        self.logger.debug(f"Getting center frequency of {self.gpib_address!r}")
+        response = self.query("CF?")
+        return float(response)
+
+    def set_span(self, span: float):
+        """Set the span of the spectrum analyzer in Hertz."""
+        self.logger.debug(f"Setting span of {self.gpib_address!r} to {span=}")
+        self.write(f"SP {span}")
+
+    def get_span(self) -> float:
+        """Get the span of the spectrum analyzer in Hertz."""
+        self.logger.debug(f"Getting span of {self.gpib_address!r}")
+        return float(self.query("SP?").strip())
+
+    def get_lower_frequency(self) -> float:
+        """Get the lower frequency of the spectrum analyzer in Hertz."""
+        self.logger.debug(f"Getting lower frequency of {self.gpib_address!r}")
+        return float(self.query("FA?"))
+
+    def get_upper_frequency(self) -> float:
+        """Get the upper frequency of the spectrum analyzer in Hertz."""
+        self.logger.debug(f"Getting upper frequency of {self.gpib_address!r}")
+        return float(self.query("FB?"))
+
+    def set_lower_frequency(self, frequency: float):
+        """Set the lower frequency of the spectrum analyzer in Hertz."""
+        self.logger.debug(f"Setting lower frequency of {self.gpib_address!r} to {frequency=}")
+        self.write(f"FA {frequency}")
+
+    def set_upper_frequency(self, frequency: float):
+        """Set the upper frequency of the spectrum analyzer in Hertz."""
+        self.logger.debug(f"Setting upper frequency of {self.gpib_address!r} to {frequency=}")
+        self.write(f"FB {frequency}")
+
+    def get_trace(self, trace: Literal["A", "B"] = "A") -> np.ndarray[np.float64]:
+        """Get the trace from the spectrum analyzer."""
+        self.logger.debug(f"Getting trace {trace} from {self.gpib_address!r}")
+        response = self.query(f"TR{trace}?")
+        return np.array([float(x) for x in response.split(",")])
+
+    def get_trace_frequencies_and_amplitudes(
+        self, trace: Literal["A", "B"] = "A"
+    ) -> tuple[np.ndarray[np.float64], np.ndarray[np.float64]]:
+        """Get the frequencies and amplitudes of the trace from the spectrum analyzer.
+
+        Response will be a tuple of Numpy arrays: `(frequencies, amplitudes)`."""
+        import numpy as np
+
+        self.logger.debug(f"Getting trace frequencies and amplitudes {trace} from {self.gpib_address!r}")
+        amplitudes = self.get_trace(trace)
+        # Have to manually calculate the frequencies because the spectrum analyzer doesn't provide them.
+        frequencies = np.linspace(self.get_lower_frequency(), self.get_upper_frequency(), len(amplitudes))
+        return frequencies, amplitudes
+
+    def get_marker_frequency(self) -> float:
+        """Get the frequency of the marker in Hertz."""
+        self.logger.debug(f"Getting marker frequency from {self.gpib_address!r}")
+        return float(self.query(f"MKF?"))
+
+    def set_marker_frequency(self, frequency: float):
+        """Set the frequency of the marker in Hertz."""
+        self.logger.debug(f"Setting marker frequency of {self.gpib_address!r} to {frequency=}")
+        self.write(f"MKF {frequency}")
+
+    def get_marker_amplitude(self) -> float:
+        """Get the amplitude of the marker."""
+        self.logger.debug(f"Getting marker amplitude from {self.gpib_address!r}")
+        return float(self.query(f"MKA?"))
+
+    def get_center_frequency_amplitude(self) -> float:
+        """Get the amplitude at the center frequency of the spectrum analyzer.
+
+        Does three things:
+        1. Gets the center frequency.
+        2. Sets the marker frequency to the center frequency.
+        3. Gets the marker amplitude."""
+        self.logger.debug(f"Getting center frequency amplitude from {self.gpib_address!r}")
+        center_frequency = self.get_center_frequency()
+        self.set_marker_frequency(center_frequency)
+        return self.get_marker_amplitude()
 
 
 if __name__ == "__main__":
@@ -223,13 +286,17 @@ if __name__ == "__main__":
     spec_an_logger = ssc_log.logger.getChild("spec_an")
     spec_an_logger.info(f"Starting {__file__}")
 
-    if True:
-        spectrum_analyzer = SpectrumAnalyzerHP8563E.find()
-
-    # with SpectrumAnalyzerHP8563E(
-    #     gpib_address="GPIB0::18::INSTR",
-    #     logger=spec_an_logger,
-    # ) as spectrum_analyzer:
+    with SpectrumAnalyzerHP8563E.find(
+        logger=spec_an_logger,
+    ) as spectrum_analyzer:
+        print(f"{spectrum_analyzer=} {type(spectrum_analyzer)=} {bool(spectrum_analyzer)=}")
+        if not spectrum_analyzer:
+            spec_an_logger.error("No spectrum analyzer found.")
+            sys.exit(1)
+        # with SpectrumAnalyzerHP8563E(
+        #     gpib_address="GPIB0::18::INSTR",
+        #     logger=spec_an_logger,
+        # ) as spectrum_analyzer:
         # spectrum_analyzer.open()
 
         print(spectrum_analyzer.query("CF?"))
@@ -278,7 +345,10 @@ if __name__ == "__main__":
 
         xs, ys = spectrum_analyzer.get_trace_frequencies_and_amplitudes("A")
 
+        print(f"{spectrum_analyzer.query('AUNITS?')}")
+
         import matplotlib.pyplot as plt
+
         fig, ax = plt.subplots()
         ax.plot(xs, ys)
         ax.set_xlabel("Frequency (Hz)")
