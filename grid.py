2020d36ffa759748674659e77f1b47bd077ca0f0
David Mayo <dcmayo@gmail.com>
2025-02-19 01:19:23 -0500

    Add grid stuff

---
+++ grid.py
@@ -0,0 +1,218 @@
+from __future__ import annotations
+
+from typing import TYPE_CHECKING
+from typing import Literal
+
+import numpy as np
+
+from msu_anechoic import AzEl
+
+if TYPE_CHECKING:
+    import matplotlib.pyplot as plt
+
+
+def estimated_step_time(
+    *,
+    azimuth: float,
+    elevation: float,
+) -> float:
+    """Estimate the time it will take to move a given step in  azimuth and elevation.
+
+    This function is a simple linear model based on the data in `turntable_move_times.csv`.
+    These values were empirically measured by David Mayo in February 2025.
+
+    IMPORTANT: If azimuth or elevation is less than 0.1, it is treated as not moving. This reflects the turntable firmware code,
+    where that 0.1 margin is hardcoded.
+    """
+    azimuth_time = 0.0
+    elevation_time = 0.0
+    if abs(azimuth) > 0.1:
+        azimuth_time = 0.39 * abs(azimuth) + 2.71
+    if abs(elevation) > 0.1:
+        elevation_time = 0.94 * abs(elevation) + 3.25
+    return max(azimuth_time, elevation_time)
+
+
+class GridPattern:
+    def __init__(
+        self,
+        *,
+        azimuth_min: float,
+        azimuth_max: float,
+        elevation_min: float,
+        elevation_max: float,
+        azimuth_step_count: float,
+        elevation_step_count: float,
+        start_corner: Literal["UL", "UR", "LL", "LR"],
+        initial_position: AzEl | None = None,
+        direction: Literal["HORIZONTAL", "VERTICAL"],
+    ):
+        self.azimuth_min = azimuth_min
+        self.azimuth_max = azimuth_max
+        self.elevation_min = elevation_min
+        self.elevation_max = elevation_max
+        self.azimuth_step_count = azimuth_step_count
+        self.elevation_step_count = elevation_step_count
+        self.direction = direction
+
+        self.points: list[AzEl] = []
+        self.initial_position = initial_position
+        self.azimuth_values = [float(x) for x in np.linspace(self.azimuth_min, self.azimuth_max, self.azimuth_step_count)]
+        self.elevation_values = [float(x) for x in np.linspace(self.elevation_min, self.elevation_max, self.elevation_step_count)]
+
+        self.start_corner = start_corner
+        if self.start_corner[0] == "U":
+            self.elevation_values.reverse()
+        if self.start_corner[1] == "R":
+            self.azimuth_values.reverse()
+
+        if self.direction == "HORIZONTAL":
+            for elevation_index, elevation in enumerate(self.elevation_values):
+                row_azimuths = self.azimuth_values[:]
+                if elevation_index % 2 == 1:
+                    row_azimuths.reverse()
+                for azimuth_index, azimuth in enumerate(row_azimuths):
+                    self.points.append(AzEl(azimuth, elevation))
+        elif self.direction == "VERTICAL":
+            for azimuth_index, azimuth in enumerate(self.azimuth_values):
+                column_elevations = self.elevation_values[:]
+                if azimuth_index % 2 == 1:
+                    column_elevations.reverse()
+                for elevation_index, elevation in enumerate(column_elevations):
+                    self.points.append(AzEl(azimuth, elevation))
+        else:
+            raise ValueError(f"Invalid direction: {self.direction}")
+
+    def height(self) -> float:
+        return abs(self.elevation_max - self.elevation_min)
+    
+    def width(self) -> float:
+        return abs(self.azimuth_max - self.azimuth_min)
+    
+    def elevation_step_size(self) -> float:
+        if self.elevation_step_count == 1:
+            return 0
+        return self.height() / (self.elevation_step_count - 1)
+    
+    def azimuth_step_size(self) -> float:
+        if self.azimuth_step_count == 1:
+            return 0
+        return self.width() / (self.azimuth_step_count - 1)
+    
+    def elevation_steps(self) -> int:
+        if self.direction == "HORIZONTAL":
+            return self.elevation_step_count - 1
+        elif self.direction == "VERTICAL":
+            total_steps = self.azimuth_step_count * self.elevation_step_count
+            return total_steps - self.azimuth_step_count
+        
+    def azimuth_steps(self) -> int:
+        if self.direction == "HORIZONTAL":
+            total_steps = self.azimuth_step_count * self.elevation_step_count
+            return total_steps - self.elevation_step_count
+        elif self.direction == "VERTICAL":
+            return self.azimuth_step_count - 1
+        
+    def azimuth_step_time(self) -> float:
+        return estimated_step_time(azimuth=self.azimuth_step_size(), elevation=0)
+    
+    def elevation_step_time(self) -> float:
+        return estimated_step_time(azimuth=0, elevation=self.elevation_step_size())
+    
+    def total_azimuth_time(self) -> float:
+        return self.azimuth_steps() * self.azimuth_step_time()
+    
+    def total_elevation_time(self) -> float:
+        return self.elevation_steps() * self.elevation_step_time()
+    
+    def total_grid_time(self) -> float:
+        return self.total_azimuth_time() + self.total_elevation_time()
+
+    def plot(self, ax: "plt.Axes"):
+        azimuths = [point.azimuth for point in self.points]
+        elevations = [point.elevation for point in self.points]
+        ax.plot(azimuths, elevations, label="Grid Pattern", marker="o", color="black")
+
+        first_point = self.points[0]
+        last_point = self.points[-1]
+
+        for point_index, point in enumerate(self.points):
+            ax.text(
+                point.azimuth,
+                point.elevation,
+                f"{point_index}",
+                # fontsize=8,
+                # ha="center",
+                # va="center",
+            )
+
+
+
+        if self.initial_position:
+            ax.plot(
+                [self.initial_position.azimuth, first_point.azimuth],
+                [self.initial_position.elevation, first_point.elevation],
+                # label="Start Position",
+                # marker="o",
+                linestyle="--",
+                color="black",
+            )
+        ax.scatter(first_point.azimuth, first_point.elevation, label="First Point", marker="o", color="#00ff0088", zorder=10, s=300)
+        ax.scatter(last_point.azimuth, last_point.elevation, label="Last Point", marker="o", color="#ff000088", zorder=10, s=300)
+        ax.set_xlabel("Azimuth (degrees)")
+        ax.set_ylabel("Elevation (degrees)")
+        # ax.set_xlabel("Azimuth (degrees)")
+        # ax.set_ylabel("Elevation (degrees)")
+        # ax.grid(which="both")
+
+
+if __name__ == "__main__":
+    fine_grid = GridPattern(
+        azimuth_min=-20,
+        azimuth_max=20,
+        elevation_min=-20,
+        elevation_max=20,
+        azimuth_step_count=41,
+        elevation_step_count=41,
+        start_corner="UL",
+        # initial_position=AzEl(0, 0),
+        # direction="VERTICAL",
+        direction="HORIZONTAL",
+    )
+    coarse_grid = GridPattern(
+        azimuth_min=-180,
+        azimuth_max=180,
+        elevation_min=-20,
+        elevation_max=20,
+        azimuth_step_count=361,
+        elevation_step_count=21,
+        start_corner="UL",
+        # initial_position=AzEl(0, 0),
+        # direction="VERTICAL",
+        direction="HORIZONTAL",
+    )
+    grid = fine_grid
+    grid = coarse_grid
+    print(grid.azimuth_values)
+    print(grid.elevation_values)
+    print(grid.points)
+    print("Done")
+
+    import matplotlib.pyplot as plt
+    fig, ax= plt.subplots()
+    grid.plot(ax)
+    print(f"{grid.height()=}")
+    print(f"{grid.width()=}")
+    print(f"{grid.elevation_step_size()=}")
+    print(f"{grid.azimuth_step_size()=}")
+    print(f"{grid.elevation_steps()=}")
+    print(f"{grid.azimuth_steps()=}")
+    print(f"{grid.azimuth_step_time()=}")
+    print(f"{grid.elevation_step_time()=}")
+    print(f"{grid.total_azimuth_time()=}")
+    print(f"{grid.total_elevation_time()=}")
+    print(f"{grid.total_grid_time()        = }")
+    print(f"{grid.total_grid_time() / 60   = }")
+    print(f"{grid.total_grid_time() / 3600 = }")
+    
+    plt.show()
