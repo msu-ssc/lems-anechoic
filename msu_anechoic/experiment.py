f4e391e48db92349ab47cf81d6416b98b517b4f2
David Mayo <dcmayo@moreheadstate.edu>
2025-02-25 18:11:25 -0500

    More work

--- msu_anechoic/experiment.py
+++ msu_anechoic/experiment.py
@@ -36,7 +36,9 @@ class ExperimentMetadata(pydantic.BaseModel):
 
     relative_folder_path: Path | None = None
     raw_data_csv_path: Path | None = None
-    metadata_path: Path | None = None
+    metadata_json_path: Path | None = None
+    log_jsonl_path: Path | None = None
+    log_plaintext_path: Path | None = None
 
     grid: Grid | None = None
     points: list[AzElTurntable] | None = None
@@ -49,8 +51,8 @@ class ExperimentMetadata(pydantic.BaseModel):
     actual_start_spec_an_config: SpecAnConfig | None = None
     actual_finish_spec_an_config: SpecAnConfig | None = None
     
-    experiment_start_time_utc: datetime.datetime | None = None
-    experiment_finish_time_utc: datetime.datetime | None = None
+    # experiment_start_time_utc: datetime.datetime | None = None
+    # experiment_finish_time_utc: datetime.datetime | None = None
     points: dict[int, AzElTurntable] | None = None
     # az_el_cls: Literal["AzElSpherical", "AzElTurntable"] | None = None
 
@@ -60,28 +62,37 @@ class ExperimentMetadata(pydantic.BaseModel):
             self.relative_folder_path = path_util.clean_path(EXPERIMENTS_FOLDER_PATH / self.short_description)
         if self.raw_data_csv_path is None:
             self.raw_data_csv_path = self.relative_folder_path / "raw_data.csv"
-        if self.metadata_path is None:
-            self.metadata_path = self.relative_folder_path / "experiment_metadata.json"
+        if self.metadata_json_path is None:
+            self.metadata_json_path = self.relative_folder_path / "experiment_metadata.json"
+        if self.log_jsonl_path is None:
+            self.log_jsonl_path = self.relative_folder_path / "logs" / "log.jsonl"
+        if self.log_plaintext_path is None:
+            self.log_plaintext_path = self.relative_folder_path / "logs" / "log.txt"
+
+        if self.grid is not None and self.points is not None:
+            raise ValueError("Cannot have both `grid` and `points`")
+        elif self.grid is None and self.points is None:
+            raise ValueError("Must have either `grid` or `points`")
 
     # @property
     # def experiment_folder_path(self) -> Path:
     #     return path_util.clean_path(EXPERIMENTS_FOLDER_PATH / self.short_description).expanduser().resolve()
 
     def write_metadata(self) -> None:
-        parent = self.metadata_path.parent
+        parent = self.metadata_json_path.parent
         parent.mkdir(parents=True, exist_ok=True)
-        self.metadata_path.write_text(self.model_dump_json(indent=4))
+        self.metadata_json_path.write_text(self.model_dump_json(indent=4))
 
 
 if __name__ == "__main__":
     spec_an_config = SpecAnConfig(center_frequency=8_450_000_000, span=1_000)
     sig_gen_config = SigGenConfig(center_frequency=8_450_000_000, power=-10, vernier_power=0)
-    points = {
-        0: AzElTurntable(azimuth=-5, elevation=5),
-        1: AzElTurntable(azimuth=5, elevation=5),
-        # 2: AzElTurntable(azimuth=-5, elevation=-5),
-        # 3: AzElTurntable(azimuth=5, elevation=-5),
-    }
+    # points = {
+    #     0: AzElTurntable(azimuth=-5, elevation=5),
+    #     1: AzElTurntable(azimuth=5, elevation=5),
+    #     # 2: AzElTurntable(azimuth=-5, elevation=-5),
+    #     # 3: AzElTurntable(azimuth=5, elevation=-5),
+    # }
     grid = Grid(
         min_azimuth=-10,
         max_azimuth=10,
